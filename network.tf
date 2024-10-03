242f8964903632e3c9936bf89b35bdb997049690
John Gomes <silva.johngomes@gmail.com>
2024-10-03 20:29:28 -0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,3 +4,8 @@ resource "aws_internet_gateway" "prod-igw" {
         Name = "prod-igw"
     }
 }
+
+resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
